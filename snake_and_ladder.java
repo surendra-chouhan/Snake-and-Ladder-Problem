f042d1c8266e3e4062212a5481ba55f18934cb84
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-22 21:50:57 +0530

    Refactored the code to play with 2 players and show the winner among them

--- snake_and_ladder.java
+++ snake_and_ladder.java
@@ -3,51 +3,58 @@ public class snake_and_ladder {
 	public static final int start_position = 0;
 	public static final int winning_position = 100;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Snake and Ladder Game");
-
+	public int total_die_rolls(){
 		int roll_index = 0;
 		int player_position = start_position;
-		System.out.println("Starting position of a player is : "+ player_position);
+
 
 		while (player_position < 100 && player_position >=0 ) {
 			int die_roll = (int)(Math.floor(Math.random() * 10 ) % 6) + 1;
-			System.out.println("The value of a die rolled is : " + die_roll);
 
 			roll_index++;
-
 			player_position += die_roll;
-			System.out.println("Player 1 position is : " + player_position);
 
 			int option = (int)Math.floor(Math.random() * 10 ) % 3;
-			System.out.println("Option is : " + option);
 
 			if (option == 1) {
 				player_position += 0;
-				System.out.println("Player 1 stays in same position : " + player_position);
 			}
 			else if (option == 2) {
 				player_position += die_roll;
-				System.out.println("Player got a ladder. Position is : " + player_position );
 			}
 			else {
 				player_position -= die_roll;
-				System.out.println("Player got a Snake. Position is : " + player_position);
+
 				if (player_position < 0){
-                                       	player_position = 0;
-                               	}
-
+	                               	player_position = 0;
+				}
 			}
-
 			if (player_position>100)
 			{
 				player_position -= player_position;
 			}
-
-			System.out.println("For die roll " + roll_index + " position is : " + player_position);
-
 		}
-		System.out.println("Player 1 won!!");
-		System.out.println("Total number of times the die was rolled is : " + roll_index);
+		return roll_index;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and Ladder Game");
+
+		snake_and_ladder sl = new snake_and_ladder();
+		int player1_rollindex = sl.total_die_rolls();
+
+		System.out.println("Total number of times the die was rolled for player 1 is : " + player1_rollindex);
+
+		snake_and_ladder sl2 = new snake_and_ladder();
+		int player2_rollindex = sl2.total_die_rolls();
+		System.out.println("Total number of times the die was rolled for player 2 is : " + player2_rollindex);
+
+		if ( player1_rollindex == player2_rollindex )
+			System.out.println("It is a tie!!!");
+		else if (player1_rollindex > player2_rollindex)
+			System.out.println("Player 2 is the winner!!!");
+		else
+			System.out.println("Player 1 is the winner!!!");
+
 	}
 }
