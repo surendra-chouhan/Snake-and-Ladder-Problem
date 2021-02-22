d724ec291a10bae4be5fc006ee5549f3ab5fcf0c
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-22 20:24:52 +0530

    Added code to check for No Play, Ladder or Snake

--- snake_and_ladder.java
+++ snake_and_ladder.java
@@ -11,8 +11,24 @@ public class snake_and_ladder {
 		int die_roll = (int)(Math.floor(Math.random() * 10 ) % 6) + 1;
 		System.out.println("The value of a die rolled is : " + die_roll);
 
+		player_position += die_roll;
+		System.out.println("Player 1 position is : " + player_position);
 
+		int option = (int)Math.floor(Math.random() * 10 ) % 3;
+		System.out.println("Option is : " + option);
 
+		if (option == 1) {
+			player_position += 0;
+			System.out.println("Player 1 stays in same position : " + player_position);
+		}
+		else if (option == 2) {
+			player_position += die_roll;
+			System.out.println("Player got a ladder. Position is : " + player_position );
+		}
+		else {
+			player_position -= die_roll;
+			System.out.println("Player got a Snake. Position is : " + player_position);
+		}
 	}
 
 }
