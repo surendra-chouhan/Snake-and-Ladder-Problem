84ed05d556e649b98ff4586320173d947b91af4a
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-22 20:00:11 +0530

    Added code to start from position 0

--- snake_and_ladder.java
+++ snake_and_ladder.java
@@ -1,6 +1,13 @@
 public class snake_and_ladder {
 
+	public static final int start_position = 0;
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game");
+
+		int player_position = start_position;
+		System.out.println("Starting position of a player is : "+ player_position);
+
 	}
+
 }
