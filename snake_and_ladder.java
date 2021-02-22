72ade2f6b436817ad3a893615d00aa83ae1a5325
surendra-chouhan <chouhansurendra028@gmail.com>
2021-02-22 20:37:01 +0530

    Added code to repeat till winning position is reached

--- snake_and_ladder.java
+++ snake_and_ladder.java
@@ -1,6 +1,7 @@
 public class snake_and_ladder {
 
 	public static final int start_position = 0;
+	public static final int winning_position = 100;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder Game");
@@ -8,27 +9,38 @@ public class snake_and_ladder {
 		int player_position = start_position;
 		System.out.println("Starting position of a player is : "+ player_position);
 
-		int die_roll = (int)(Math.floor(Math.random() * 10 ) % 6) + 1;
-		System.out.println("The value of a die rolled is : " + die_roll);
+		while (player_position < 100 && player_position >=0 ) {
+			int die_roll = (int)(Math.floor(Math.random() * 10 ) % 6) + 1;
+			System.out.println("The value of a die rolled is : " + die_roll);
 
-		player_position += die_roll;
-		System.out.println("Player 1 position is : " + player_position);
-
-		int option = (int)Math.floor(Math.random() * 10 ) % 3;
-		System.out.println("Option is : " + option);
-
-		if (option == 1) {
-			player_position += 0;
-			System.out.println("Player 1 stays in same position : " + player_position);
-		}
-		else if (option == 2) {
 			player_position += die_roll;
-			System.out.println("Player got a ladder. Position is : " + player_position );
-		}
-		else {
-			player_position -= die_roll;
-			System.out.println("Player got a Snake. Position is : " + player_position);
+			System.out.println("Player 1 position is : " + player_position);
+
+			int option = (int)Math.floor(Math.random() * 10 ) % 3;
+			System.out.println("Option is : " + option);
+
+			if (option == 1) {
+				player_position += 0;
+				System.out.println("Player 1 stays in same position : " + player_position);
+			}
+			else if (option == 2) {
+				player_position += die_roll;
+				System.out.println("Player got a ladder. Position is : " + player_position );
+			}
+			else {
+				player_position -= die_roll;
+				System.out.println("Player got a Snake. Position is : " + player_position);
+				if (player_position < 0){
+                                       	player_position = 0;
+                               	}
+
+			}
+
+			if (player_position>100)
+			{
+				player_position -= player_position;
+			}
+
 		}
 	}
-
 }
